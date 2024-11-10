3f167d48e03050add565feb7a2bb16d7c6d5b35b
FillipeMendesC <fillipe.coelho@sptech.school>
2024-11-10 19:24:47 -0300

    Adição de setas na cruzadinha

--- caos.js
+++ caos.js
@@ -1,9 +1,9 @@
-var respostas   = ['CPU', 'ULA', 'REGISTRADORES', 'RAM', 'ROM', 'EPROM', 'FLASH', 'MEMORIADEMASSA', 'DMA', 'CS', 'ADDRESSBUS', 'DATABUS', 'I5', 'I7', 'DUALCORE', 'QUADCORE'];
-var resPosition = [[0,5,6],[0,12,15],[0,6,7],[1,14,5],[0,2,15],[1,7,4],[0,15,15],[1,17,2],[1,2,2],[0,14,16],[1,11,0],[0,8,16],[0,7,17],[1,10,6],[1,21,7],[0,4,0]]
+var respostas   = ['CPU',  'ULA',    'REGISTRADORES', 'RAM',   'ROM',   'EPROM', 'FLASH',  'MEMORIADEMASSA', 'DMA',  'CS',     'ADDRESSBUS', 'DATABUS', 'I5',    'I7',    'DUALCORE', 'QUADCORE'];
+var resPosition = [[0,5,6],[0,13,14],[0,6,7],         [1,14,5],[0,3,14],[1,8,3], [0,15,13],[1,17,2],         [1,2,2],[0,14,15],[1,12,0],     [0,8,15],  [0,7,16],[1,11,5],[1,21,6],   [0,4,0]]
 
 var questao = 0
 
-function gerarCruzadinha(coluna, linha) {
+function gerarCruzadinha(linha, coluna) {
     var matrix = []
     for (let i = 0; i < linha; i++){
         matrix[i] = []
@@ -44,8 +44,11 @@ function colocarEspacos() {
             getCell(linha, coluna).classList.add('in');
         };
 
-        for (let block = 0; block < resposta.length; block++) {
-            if (orientacao === 0) {
+        console.log(i+1, linhaInicial, colunaInicial, resposta)
+
+        arrowCell(orientacao, i, linhaInicial, colunaInicial)
+        for (let block = 1; block <= resposta.length; block++) {
+            if (orientacao == 0) {
                 changeCell(linhaInicial, colunaInicial + block);
             } else {
                 changeCell(linhaInicial + block, colunaInicial);
@@ -54,9 +57,16 @@ function colocarEspacos() {
     }
 }
 
+function arrowCell(arrowDirection, numPes, linha, coluna){
+    const arrowClass = ['⇒', '⇓']
+    const cell = getCell(linha, coluna)
+    cell.classList.add('arrow')
+    cell.innerHTML = `${numPes+1}${arrowClass[arrowDirection]}`
+}
 
-function getCell(line, column){
-    return cruzadinha.childNodes[line].childNodes[column]
+
+function getCell(linha, coluna){
+    return cruzadinha.childNodes[linha].childNodes[coluna]
 }
 
 function colocarNaCruzadinha(correto) {
@@ -73,14 +83,12 @@ function colocarNaCruzadinha(correto) {
 
     for (let block = 0; block < correto.length; block++) {
         if (orientacao === 0) {
-            addCharCell(linhaInicial, colunaInicial + block, correto[block]);
+            addCharCell(linhaInicial, colunaInicial + block+1, correto[block]);
         } else {
-            addCharCell(linhaInicial + block, colunaInicial, correto[block]);
+            addCharCell(linhaInicial + block+1, colunaInicial, correto[block]);
         }
     }
 }
-
-
 
 function verificar(inp){
     var respostaSemEspaco = ''
@@ -99,10 +107,18 @@ function verificar(inp){
     }
 }
 
+function secret(){
+    for(let i = 0; i < respostas.length; i++){
+        colocarNaCruzadinha(respostas[i])
+    }
+}
+
 
 function iniciarJogo(){
     btn_jogar.style.display = 'none'
     div_instrucao.style.display = 'none'
     jogo.style.display = 'flex'
-    mostrarCruzadinha(gerarCruzadinha(23,16))
+    mostrarCruzadinha(gerarCruzadinha(17,24))
+    // remover
+    secret()
 }
